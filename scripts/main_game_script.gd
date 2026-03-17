2d48d1053abae8f36ff22f8c6c62602a2d16fffa
Ankit Suman <ankitsuman07@gmail.com>
2026-03-17 19:24:29 -0400

    Add winning logic

--- scripts/main_game_script.gd
+++ scripts/main_game_script.gd
@@ -27,6 +27,37 @@ func _on_button_click(idx, button):
 		current_player = CELL_X if current_player == CELL_0 else CELL_0
 		button.disabled = true
 		print(board)
+		var winner = get_winner(board)
+
+		if winner != null:
+			print("Winner is:", winner)
+		
+func get_winner(board):
+	# Check rows
+	for i in range(3):
+		var v = board[i][0].text
+		if v != "" and v == board[i][1].text and v == board[i][2].text:
+			return v
+	
+	# Check columns
+	for j in range(3):
+		var v = board[0][j].text
+		if v != "" and v == board[1][j].text and v == board[2][j].text:
+			return v
+	
+	# Check diagonals
+	var center = board[1][1].text
+	
+	if center != "":
+		# Main diagonal
+		if center == board[0][0].text and center == board[2][2].text:
+			return center
+		
+		# Anti-diagonal
+		if center == board[0][2].text and center == board[2][0].text:
+			return center
+	
+	return null  # No winner yet
 
 func reset_game():
 	current_player = CELL_X
