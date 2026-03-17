3c0b8ca3f9bfe3a109725ec9fc669f24d9262fd4
Ankit Suman <ankitsuman07@gmail.com>
2026-03-17 15:56:23 -0400

    Create game script

---
+++ scripts/main_game_script.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var button = $GridContainer.get_children()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
