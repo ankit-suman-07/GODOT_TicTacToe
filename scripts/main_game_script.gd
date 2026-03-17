016516592b368335669d00cecc5fb610f91e7403
Ankit Suman <ankitsuman07@gmail.com>
2026-03-17 16:34:07 -0400

    Code to update cell

--- scripts/main_game_script.gd
+++ scripts/main_game_script.gd
@@ -1,7 +1,37 @@
 extends Node2D
 
-@onready var button = $GridContainer.get_children()
+const CELL_EMPTY = ""
+const CELL_X = "X"
+const CELL_0 = "0"
+
+@onready var buttons = $GridContainer.get_children()
+
+var current_player
+var board
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var button_index = 0
+	for button in buttons:
+		button.connect("pressed", _on_button_click.bind(button_index, button))
+		button_index += 1
+	reset_game()
+		
+func _on_button_click(idx, button):
+	var y = idx / 3
+	var x = idx % 3
+	button.text = current_player
+	if board[y][x] == CELL_EMPTY:
+		board[y][x] = current_player
+	
+		current_player = CELL_X if current_player == CELL_0 else CELL_0
+		button.disabled = true
+		print(board)
+
+func reset_game():
+	current_player = CELL_X
+	board = [
+		[CELL_EMPTY, CELL_EMPTY, CELL_EMPTY],
+		[CELL_EMPTY, CELL_EMPTY, CELL_EMPTY],
+		[CELL_EMPTY, CELL_EMPTY, CELL_EMPTY]
+	]
