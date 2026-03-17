fa1c09f441be56d402142cb52fbcc8e091cc16cf
Ankit Suman <ankitsuman07@gmail.com>
2026-03-17 19:28:09 -0400

    Add draw option

--- scripts/main_game_script.gd
+++ scripts/main_game_script.gd
@@ -9,60 +9,100 @@ const CELL_0 = "0"
 var current_player
 var board
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
 	var button_index = 0
 	for button in buttons:
 		button.connect("pressed", _on_button_click.bind(button_index, button))
 		button_index += 1
+	
 	reset_game()
-		
+
+
 func _on_button_click(idx, button):
 	var y = idx / 3
 	var x = idx % 3
-	button.text = current_player
-	if board[y][x] == CELL_EMPTY:
-		board[y][x] = current_player
 	
-		current_player = CELL_X if current_player == CELL_0 else CELL_0
-		button.disabled = true
-		print(board)
-		var winner = get_winner(board)
+	# Prevent overwriting moves
+	if board[y][x] != CELL_EMPTY:
+		return
+	
+	# Update logic
+	board[y][x] = current_player
+	
+	# Update UI
+	button.text = current_player
+	button.disabled = true
+	
+	# Check winner
+	var winner = get_winner()
+	if winner != null:
+		print("Winner is:", winner)
+		disable_all_buttons()
+		return
+	
+	# Check draw
+	if is_draw():
+		print("Draw!")
+		disable_all_buttons()
+		return
+	
+	# Switch player
+	current_player = CELL_X if current_player == CELL_0 else CELL_0
+	
+	print(board)
 
-		if winner != null:
-			print("Winner is:", winner)
-		
-func get_winner(board):
+
+func get_winner():
 	# Check rows
 	for i in range(3):
-		var v = board[i][0].text
-		if v != "" and v == board[i][1].text and v == board[i][2].text:
+		var v = board[i][0]
+		if v != "" and v == board[i][1] and v == board[i][2]:
 			return v
 	
 	# Check columns
 	for j in range(3):
-		var v = board[0][j].text
-		if v != "" and v == board[1][j].text and v == board[2][j].text:
+		var v = board[0][j]
+		if v != "" and v == board[1][j] and v == board[2][j]:
 			return v
 	
 	# Check diagonals
-	var center = board[1][1].text
+	var center = board[1][1]
 	
 	if center != "":
 		# Main diagonal
-		if center == board[0][0].text and center == board[2][2].text:
+		if center == board[0][0] and center == board[2][2]:
 			return center
 		
 		# Anti-diagonal
-		if center == board[0][2].text and center == board[2][0].text:
+		if center == board[0][2] and center == board[2][0]:
 			return center
 	
-	return null  # No winner yet
+	return null
+
+
+func is_draw():
+	for row in board:
+		for cell in row:
+			if cell == CELL_EMPTY:
+				return false
+	return true
+
+
+func disable_all_buttons():
+	for button in buttons:
+		button.disabled = true
+
 
 func reset_game():
 	current_player = CELL_X
+	
 	board = [
 		[CELL_EMPTY, CELL_EMPTY, CELL_EMPTY],
 		[CELL_EMPTY, CELL_EMPTY, CELL_EMPTY],
 		[CELL_EMPTY, CELL_EMPTY, CELL_EMPTY]
 	]
+	
+	for button in buttons:
+		button.text = ""
+		button.disabled = false
